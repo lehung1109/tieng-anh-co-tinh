6c283bffc93004b5769266d4fec87834e4574bbd
Hung Le <hung.le.ext@ericsson.com>
2025-12-23 21:49:03 +0700

    Refactor Header and Logo components: standardize formatting and add missing semicolons for consistency.

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,13 +1,19 @@
-import Link from "next/link"
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 const Logo = () => {
   return (
     <Link href="/">
-          <Image className="hidden" src="/logo.png" alt="logo" width={223} height={69} />
-          Logo
-        </Link>
-  )
-}
+      <Image
+        className="hidden"
+        src="/logo.png"
+        alt="logo"
+        width={223}
+        height={69}
+      />
+      Logo
+    </Link>
+  );
+};
 
 export default Logo;
