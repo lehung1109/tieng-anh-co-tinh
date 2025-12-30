6a5298efcf55dbe5225ca431138b7130b42e3564
Hung Le <hung0895@gmail.com>
2025-12-30 17:43:14 +0700

    Update email sending configuration in contact form: replace hardcoded Gmail service with environment variables for host, port, and secure settings to improve flexibility and security.

--- funcs/contact-form-function.ts
+++ funcs/contact-form-function.ts
@@ -62,7 +62,9 @@ export const contactFormFunction = async (
   // Send email with nodemailer
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
