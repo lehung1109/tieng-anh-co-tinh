2cccd05e7d62c76118ee103e90cd5bc18b020c49
Hung Le <hung0895@gmail.com>
2026-03-02 17:34:42 +0700

    Enhance install prompt component: add localized install prompt description, improve layout with image and button, and integrate translation support for better user experience.

--- components/install-prompt/default-prompt.tsx
+++ components/install-prompt/default-prompt.tsx
@@ -1,11 +1,32 @@
+"use client";
+
+import { useT } from "@/lib/i18n/client";
+import { Button } from "../ui/button";
+import Image from "next/image";
+
 const DefaultPrompt = ({
   triggerInstall,
 }: {
   triggerInstall: () => Promise<void>;
 }) => {
+  const { t } = useT();
+
   return (
-    <div>
-      <h1>Default Prompt</h1>
+    <div className="flex flex-col gap-4 items-center justify-center fixed p-4 bg-gray-950 shadow-lg rounded-lg top-30 right-10 max-w-sm z-50 text-center">
+      <div className="text-2xl font-bold flex items-center justify-center gap-4">
+        <Image
+          className="rounded-full w-10 h-10 object-cover"
+          src="/icon-large.png"
+          alt="logo"
+          width={50}
+          height={50}
+        />
+        Tiếng Anh Cô Tình
+      </div>
+
+      <p>{t("common.installPromptDescription")}</p>
+
+      <Button onClick={triggerInstall}>Install</Button>
     </div>
   );
 };
